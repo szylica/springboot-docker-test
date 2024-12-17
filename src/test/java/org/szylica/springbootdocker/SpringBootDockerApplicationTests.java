70849b4e10581861365bdfd75b1687b60d8803a5
szylica <szymonsta12@onet.pl>
2024-12-17 21:49:50 +0100

    docker and springboot logic

---
+++ src/test/java/org/szylica/springbootdocker/SpringBootDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.szylica.springbootdocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
