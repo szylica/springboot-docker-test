f3540c43cffa599889a92462c4330c5d7ef5cc00
szylica <szymonsta12@onet.pl>
2024-12-18 16:29:46 +0100

    2nd commit

--- src/main/java/org/szylica/springbootdocker/SpringBootDockerApplication.java
+++ src/main/java/org/szylica/springbootdocker/SpringBootDockerApplication.java
@@ -2,12 +2,25 @@ package org.szylica.springbootdocker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.time.LocalDate;
+import java.time.LocalDateTime;
+import java.time.temporal.ChronoUnit;
 
 @SpringBootApplication
+@RestController
 public class SpringBootDockerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootDockerApplication.class, args);
-	}
+    @GetMapping("/")
+    public String getMessage() {
+        LocalDateTime localDateTime = LocalDateTime.now().truncatedTo(ChronoUnit.SECONDS);
+        return "Szymon Stalica\n351304 " + localDateTime.toString();
+    }
+    
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDockerApplication.class, args);
+    }
 
 }
